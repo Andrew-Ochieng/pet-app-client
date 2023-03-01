936b473f086923452957fff3defdf1d303e9eadf
Code-X <andrewomosh44@gmail.com>
2023-03-01 08:27:17 +0300

    add footer & navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/login' element={ <Login /> } />
+          <Route path='/signup' element={ <Signup /> } />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
